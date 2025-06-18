8995e7880462f453e93030843624ac3e06642019
njohnb <nicholas.barger.14@gmail.com>
2025-06-18 17:01:36 -0500

    added: Application can only have one running instance now. if try to run again, will open and bring launcher to front.

--- WorkflowLauncher/Program.cs
+++ WorkflowLauncher/Program.cs
@@ -1,11 +1,16 @@
 using Microsoft.Win32;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
+using System.IO.Pipes;
+using System.Threading;
+using System.Windows.Forms;
 
 namespace WorkflowLauncher
 {
     internal static class Program
     {
+        private static Mutex _mutex;
+        public static MainForm mainForm;
         
         /// <summary>
         ///  The main entry point for the application.
@@ -16,18 +21,77 @@ namespace WorkflowLauncher
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
 
-            bool createdNew;
-            using (Mutex mutex = new Mutex(true, "WorkflowLauncher_Mutex", out createdNew))
+            bool createdNew = true;
+            _mutex = new Mutex(true, "WorkflowLauncher_Mutex", out createdNew);
+            if (createdNew)
             {
-                if (!createdNew)
+                Thread pipeThread = new Thread(ListenForPipeMessages)
                 {
-                    MessageBox.Show("Workflow Launcher is already running.", "Workflow Launcher", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                    return;
+                    IsBackground = true
+                };
+                pipeThread.Start();
+                
+                ApplicationConfiguration.Initialize();
+                mainForm = new MainForm();
+                Application.Run(mainForm);
+            }
+            else
+            {
+                try
+                {
+                    // second instance - send a message to show the window
+                    using (NamedPipeClientStream client =
+                           new NamedPipeClientStream(".", "WorkflowLauncherPipe", PipeDirection.Out))
+                    {
+                        client.Connect(3000); // wait a second
+                        using (StreamWriter writer = new StreamWriter(client))
+                        {
+                            writer.WriteLine("SHOW");
+                            writer.Flush();
+                        }
+                    }
+                }
+                catch
+                {
+                    // ignored
                 }
             }
-            
-            ApplicationConfiguration.Initialize();
-            Application.Run(new MainForm());
+        }
+
+        static void ListenForPipeMessages()
+        {
+                while (true)
+                {
+                    try
+                    {
+                        using (NamedPipeServerStream pipeServer =
+                               new NamedPipeServerStream("WorkflowLauncherPipe", PipeDirection.In))
+                        {
+                            pipeServer.WaitForConnection();
+                            
+                            using (StreamReader reader = new StreamReader(pipeServer))
+                            {
+                                string message = reader.ReadLine();
+                                if (message == "SHOW" && mainForm != null)
+                                {
+                                    mainForm.Invoke(() =>
+                                    {
+
+                                        mainForm.Show();
+                                        mainForm.BringToFront();
+                                        mainForm.Activate();
+                                        if (mainForm.WindowState == FormWindowState.Minimized)
+                                            mainForm.WindowState = FormWindowState.Normal;
+                                    });
+                                }
+                            }
+                        }
+                    }
+                    catch
+                    {
+                        // log or ignore
+                    }
+                }
         }
     }
 }
